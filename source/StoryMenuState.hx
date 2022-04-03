ec06f966d2e361e50d5474f34087501a50329b4b
Dxgamer <95672691+Amogus124@users.noreply.github.com>
2022-04-03 16:58:07 -0300

    ...

--- source/StoryMenuState.hx
+++ source/StoryMenuState.hx
@@ -121,8 +121,8 @@ class StoryMenuState extends MusicBeatState
 			}
 		}
 
-		WeekData.setDirectoryFromWeek(WeekData.weeksLoaded.get(WeekData.weeksList[0]));
-		var charArray:Array<String> = WeekData.weeksLoaded.get(WeekData.weeksList[0]).weekCharacters;
+		WeekData.setDirectoryFromWeek(loadedWeeks[0]);
+		var charArray:Array<String> = loadedWeeks[0].weekCharacters;
 		for (char in 0...3)
 		{
 			var weekCharacterThing:MenuCharacter = new MenuCharacter((FlxG.width * 0.25) * (1 + char) - 150, charArray[char]);
