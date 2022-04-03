1a8bc521315d3a235a084c375d5199e7caf4094b
Dxgamer <95672691+Amogus124@users.noreply.github.com>
2022-04-03 02:47:30 -0300

    update AchievementsMenuState.hx

--- source/AchievementsMenuState.hx
+++ source/AchievementsMenuState.hx
@@ -70,16 +70,12 @@ class AchievementsMenuState extends MusicBeatState
 		add(descText);
 		changeSelection();
 
-   #if android
-   addVirtualPad(UP_DOWN, A_B);
-   #end
+                #if android
+	        addVirtualPad(UP_DOWN, B);
+                #end
 
 		super.create();
 	}
-
-  #if android
-	addVirtualPad(FULL, A_B);
-  #end
 
 	override function update(elapsed:Float) {
 		super.update(elapsed);
